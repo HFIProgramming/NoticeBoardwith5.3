95e65d99d6ddaff7fad99ae619103515ca74b013
ZihongLin <1046298030@qq.com>
2017-02-24 22:20:19 +0800

    removed duplicate

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -85,54 +85,6 @@ class VoteController extends Controller
 		}
 	}
 
-	/**
-	* Vote handler :)
-	*
-	* @TODO String type Answer still Need Further Step
-	* @param Request $request
-	* @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-	*/
-	public function voteHandler(Request $request)
-	{
-		$answers = collect(array_map('intval', explode(',', $request->answer)));  // turn string to int
-		$vote = Vote::Id($request->id);
-		$id = 0; // set default
-		$result = $this->verifyAnswers($answers, $vote);
-		if ($result === true) {  // Safety First :)
-			switch ($request->type) {  // Start Dash!
-				case 'ticket':
-				$answers->each(function ($answer) {
-					Answer::create([
-						'option_id' => $answer,
-						// 'content' => empty($answer->content) ? $answer->content : NULL,
-					]);
-				});
-				$ticket = Ticket::ticket($request->ticket)->first();
-				$ticket->is_used = 1;  // Mark as used
-				if (!$ticket->save()) {
-					abort(500); // Something goes wrong :(
-				}
-					break;
-					case 'user':
-					$answers->each(function ($answer) use ($request) {
-						Answer::create([
-							'option_id' => $answer,
-							'user_id' => $request->user()->id,
-							// 'content' => empty($answer->content) ? $answer->content : NULL,
-						]);
-					});
-					$id = $request->user()->id;
-					break;
-				}
-			$vote = $vote->first();
-			if ($vote->save()) {
-				return view('vote.success');
-			}
-			abort(500);  // Something goes wrong :(
-		} else {
-			return $result;
-		}
-
 	 /* Check whether Vote is valid !
 	 *
 	 * @param $answers
@@ -159,29 +111,6 @@ class VoteController extends Controller
 	}
 
 	/**
-	* Check whether Vote is valid !
-	*
-	* @param $answers
-	* @param $vote
-	* @return $this|bool|void
-	*/
-	private function verifyAnswers($answers, $vote)  // Notice: Depend on Model Object and Collection Object !
-	{
-		$this->checkIfRepeatingOptions($answers);
-		$this->checkIfAllFilled($answers, $vote);
-		$this->checkIfOptionsFilledMatch($answers, $vote);
-		return true;
-	}
-
-	private function checkIfRepeatingOptions($answers)
-	{
-		if ($answers->diff($answers->unique())->isEmpty()){
-			return;
-		}
-		abort(500);
-	}
-
-	/*
 	 * @param $answers
 	 * @param $vote
 	 */
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,26 +8,26 @@
 
 @section('content')
 	<div class="login-card">
-        <!--Login Card Placeholder-->
-        <div class="login-placeholder"></div>
-        <div class="row login-form">
-            <form class="col s12">
-                <h4>Welcome to NoticeBoard.</h4>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="username" type="text">
-                        <label for="username">Username</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="password" type="password">
-                        <label for="password">Password</label>
-                    </div>
-                </div>
-                <button class="login-btn btn-flat waves-effect waves-light" type="submit" name="action">Login</button>
-      			<a href="" class="forgot-password-link">FORGOT PASSWORD?</a>
-            </form>
-        </div>
-    </div>
+		<!--Login Card Placeholder-->
+		<div class="login-placeholder"></div>
+		<div class="row login-form">
+			<form class="col s12">
+				<h4>Welcome to NoticeBoard.</h4>
+				<div class="row">
+					<div class="input-field col s12">
+						<input id="username" type="text">
+						<label for="username">Username</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input id="password" type="password">
+						<label for="password">Password</label>
+					</div>
+				</div>
+				<button class="login-btn btn-flat waves-effect waves-light" type="submit" name="action">Login</button>
+				  <a href="" class="forgot-password-link">FORGOT PASSWORD?</a>
+			</form>
+		</div>
+	</div>
 @endsection
