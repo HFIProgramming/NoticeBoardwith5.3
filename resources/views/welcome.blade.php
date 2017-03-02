783acb34fffe096a5d0acc07505e8bb283eb2d91
Ethan Hu <ethan.hqc@icloud.com>
2017-03-02 18:24:54 +0800

    修复了welcome页面的问题；修复了welcome页面回复不能显示出来的问题；在storage目录加入最新可用的.sql文件

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,10 @@
 @section('searchbar')
     <nav class="search-bar">
         <div class="nav-wrapper">
-            <form id="search-form" method="get" action="https://www.bing.com/search" target="_blank"
-                  onsubmit="Bingsitesearch(this)">
+            <form id="search-form" method="get" action="https://www.bing.com/search" target="_blank" onsubmit="Bingsitesearch(this)">
                 <div class="input-field">
-                    <input id="search" name="q" type="hidden">
-                    <input id="search" name="keyword" type="search" placeholder="Search NoticeBoard" required>
-                    <label for="search"><i class="material-icons">search</i></label>
+                    <input id="search-q" name="q" type="hidden">
+                    <input id="search-key" name="keyword" type="search" placeholder="➤  Search NoticeBoard" required>
                     <i class="material-icons" onclick="do_search()">done</i>
                 </div>
             </form>
@@ -25,14 +23,15 @@
         <div class="post-card">
             <div class="card vertical post-card-content">
                 <div class="card-action">
-                    <h6 class="subheader post-header hide-on-med-and-down">{{$post->getAuthor->name}}
-                        on {{$post->created_at}}</h6>
-                    <a href="{{url('/post'.$post->id)}}">Full article</a>
+                    <div class="row post-card-heading no-margin">
+                        <div class="col l8 hide-on-med-and-down subheader" align="left"><h6>{{$post->getAuthor->name}} on {{$post->created_at}}</h6></div>
+                        <div class="col l4 right"><a href="{{url('/post'.$post->id)}}">Full article</a></div>
+                    </div>
                 </div>
                 <div class="post-user-profile">
-                    <div class="card-image"><img class="circle" src="{{ url($post->getAuthor->avatar) }}"/></div>
+                    <div class="card-image"><img class="circle" src="{{ url($post->getAuthor->avatar) }}" /></div>
                     <div class="post-header-container">
-                        <h4 class="header post-header">{{$post->title}}</h4>
+                        <h5 class="header post-header">{{$post->title}}</h5>
                     </div>
                 </div>
                 <div class="card-stacked">
@@ -44,8 +43,7 @@
                                 @break($loop->iteration == 3)
                             @endforeach
                         </div>
-                        <h6 class="subheader post-header hide-on-large-only">{{$post->getAuthor->name}}
-                            on {{$post->created_at}}</h6>
+                        <h6 class="subheader post-header hide-on-large-only">{{$post->getAuthor->name}} on {{$post->created_at}}</h6>
                         <p class="flow-text">
                             @if (strlen($clean = CleanContent($post->content))<=150)
                                 {!! $clean !!}
@@ -55,20 +53,23 @@
                         </p>
                     </div>
                     <div class="card-content post-comment-card">
-                        @if ($post->last_user != NULL)
+                        @if ($post->getLastUser != NULL)
                         <div class="card vertical post-card-content">
                             <div class="post-user-profile">
                                     <div class="card-image"><img class="circle" src="{{$post->getLastUser->avatar}}"/></div>
                                     <div class="post-header-container">
-                                        <h6 class="header post-header"><span>{{$post->getLast_user->name}}<span
-                                                        class="blue-text">commented</span></span> <br>
-                                            <span>{{$post->updated_at}}</span></h6>
+                                        <h6 class="header post-header">
+                                            <span>{{$post->getLastUser->name}}
+                                                <span class="blue-text">commented</span>
+                                            </span> 
+                                            <br>
+                                            <span>{{$post->updated_at}}</span>
+                                        </h6>
                                     </div>
                             </div>
                             <div class="card-stacked">
                                 <div class="card-content">
-                                    <p>
-                                        Total reply:{{$post->hasManyComments->count()}}</p>
+                                    <p>{{$post->hasManyComments->first()->content}}</p>
                                 </div>
                             </div>
                         </div>
