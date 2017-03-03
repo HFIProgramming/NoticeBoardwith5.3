da941582deb91f34c60288b78426913d793e726c
Ethan Hu <ethan.hqc@icloud.com>
2017-03-03 19:36:19 +0800

    Revert "Revert "新增503,500，格式：""
    This reverts commit 6db433b935912ba7a1e920a8ba0e10362b662744.

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: NeverBehave
- * Date: 16/12/30
- * Time: 上午11:33
- */
+@extends('layouts.app')
+
+@section('title')
+   Internal Server Error
+@endsection
+
+@section('content')
+    <div class="placeholder"></div>
+    <div class="row">
+        <div class="col s10 m8 push-s1 push-m2">
+            <div class="card error-card">
+                <h4 class="blue-text"><i class="material-icons pink-text">warning</i> 500: Oops... Something went wrong with our server... <br></h4>
+                @if(empty($exception->getMessage()))
+                    <h5>Contact us for more help</h5>
+                @else
+                    <h5>{{$exception->getMessage()}}</h5>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
