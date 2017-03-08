bd20bac2e693967c447fee60317a62ae929afba3
Ethan Hu <ethan.hqc@icloud.com>
2017-03-09 00:25:06 +0800

    add error display panel to vote.individual

--- app/Http/Middleware/Vote/ResultVerify.php
+++ app/Http/Middleware/Vote/ResultVerify.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use App\Ticket;
 use App\Vote;
 use Illuminate\Support\Facades\Lang;
-
 class ResultVerify
 {
 	/**
@@ -25,22 +22,18 @@ class ResultVerify
 				'process' => 'vote'
 			]),]); // No ticket or user need to login to vote.
 		}
-
 		if (empty($vote = Vote::find($request->id))) { //check if vote exists
 			return redirect('/error/custom')->withErrors(['warning' => Lang::get('vote.vote_no_found')]); // Vote No Found
 		}
-
 		if ($vote->show_result != 1){
 			return view('vote.thanks'); // Vote result set not shown
 		}
-
 		if (!empty($ticket = Ticket::ticket($request->ticket)) && ($vote->type == 1 || $vote->type == 2)) {
 			if ($ticket->isTicketUsed($vote->id)) {
 				return $next($request);  // Ticket has used for this Vote !
 			}
 			return redirect('/error/custom')->withErrors(['warning' => Lang::get('vote.voted_first')]); // Ticket is not used !
 		}
-
 		// If user login to vote, then go with this check
 		if ($user = Auth::user() && ($vote->type == 0 || $vote->type == 2)) {
 			if ($user->isUserVoted($vote->id)) {
@@ -48,7 +41,6 @@ class ResultVerify
 			}
 			return redirect('/error/custom')->withErrors(['warning' => Lang::get('vote.user_not_voted')]); // User has not voted !
 		}
-
 		return redirect('/error/custom')->withErrors(['warning' => Lang::get('vote.credential_error')]); // Credential invalid
 	}
   
