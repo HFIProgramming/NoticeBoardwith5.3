ac08a0d114638bc87cef455f12697eaf844c09e9
JohnnyCake <jianch.johnny2015@gmail.com>
2017-03-29 19:13:07 +0800

    upgrade to 5.4

--- app/Events/UpdateModelIPAddress.php
+++ app/Events/UpdateModelIPAddress.php
@@ -6,35 +6,35 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-
 class UpdateModelIPAddress
 {
-    use InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct($type,$id,$description,$ipAddress)
-    {
-        //
-	    $this->type = $type;
-	    $this->id = $id;
-	    $this->description = $description;
-	    $this->ip_address = $ipAddress;
-    }
+	/**
+	 * Create a new event instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($type,$id,$description,$ipAddress)
+	{
+		//
+		$this->type = $type;
+		$this->id = $id;
+		$this->description = $description;
+		$this->ip_address = $ipAddress;
+	}
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
+	/**
+	 * Get the channels the event should broadcast on.
+	 *
+	 * @return Channel|array
+	 */
+	public function broadcastOn()
+	{
+		return new PrivateChannel('channel-name');
+	}
 }
