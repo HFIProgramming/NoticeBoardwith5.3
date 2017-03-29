7c6f2ce3f9f02c2a65c8146af5993472cdb9e94a
Ethan Hu <ethan.hqc@icloud.com>
2017-03-29 17:42:40 +0800

    修复登陆时没有错误提示的问题 (#69)

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,7 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'password' => 'Your password is incorrect.',
+    'no_user' => 'username does not exist'
 
 ];
